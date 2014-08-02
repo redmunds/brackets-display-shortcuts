6caaf1ce350158f13e6f9b8e40ef18a8f103818f
Mikael Jorhult <mikael@jorhult.se>
2014-08-02 23:56:30 +0200

    Swedish translation.

--- nls/sv/strings.js
+++ nls/sv/strings.js
@@ -24,21 +24,22 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define */
 
-define(function (require, exports, module) {
+define({
+    //Menu
+    "MENU_SHOW_SHORTCUTS"                       : "Visa kortkommandon",
     
-    'use strict';
+    //Table
+    "TABLE_BASE_KEY"                            : "Grundtangent",
+    "TABLE_KEY_BINDING"                         : "Tangentkombination",
+    "TABLE_COMMAND_ID"                          : "Kommando-ID",
+    "TABLE_COMMAND_NAME"                        : "Kommandonamn",
+    "TABLE_ORIGIN"                              : "Ursprung",
     
-    // Code that needs to display user strings should call require("strings") to load
-    // strings.js. This file will dynamically load strings.js for the specified by bracketes.locale.
-    // 
-    // Translations for other locales should be placed in nls/<locale<optional country code>>/strings.js
-    // Localization is provided via the i18n plugin.
-    // All other bundles for languages need to add a prefix to the exports below so i18n can find them.
-    // TODO: dynamically populate the local prefix list below?
-    module.exports = {
-        root: true,
-        de: true,
-        es: true,
-        sv: true
-    };
+    //Origins
+    "ORIG_EXTENSION"                            : "Tillägg",
+    
+    //Bottom panel
+    "PANEL_TITLE"                               : "Kortkommandon",
+    "PANEL_COPY_TO_DOC"                         : "Kopiera till nuvarande dokument",
+    "PANEL_FILTER"                              : "Filtrera&hellip;"
 });
