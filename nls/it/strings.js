01fb76a4399ea4f0f7da654e41198b4aec7fc448
1nTy <federico.fini.1992@gmail.com>
2014-09-30 17:08:28 +0200

    Translation
    Italian translation added

--- nls/it/strings.js
+++ nls/it/strings.js
@@ -24,22 +24,22 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define */
 
-define(function (require, exports, module) {
+define({
+    //Menu
+    "MENU_SHOW_SHORTCUTS": "Mostra scorciatoie",
 
-    'use strict';
+    //Table
+    "TABLE_BASE_KEY": "Tasto di base",
+    "TABLE_KEY_BINDING": "Tasto aggiuntivo",
+    "TABLE_COMMAND_ID": "ID comando",
+    "TABLE_COMMAND_NAME": "Nome commando",
+    "TABLE_ORIGIN": "Origine",
 
-    // Code that needs to display user strings should call require("strings") to load
-    // strings.js. This file will dynamically load strings.js for the specified by bracketes.locale.
-    // 
-    // Translations for other locales should be placed in nls/<locale<optional country code>>/strings.js
-    // Localization is provided via the i18n plugin.
-    // All other bundles for languages need to add a prefix to the exports below so i18n can find them.
-    // TODO: dynamically populate the local prefix list below?
-    module.exports = {
-        root: true,
-        de: true,
-        es: true,
-        sv: true,
-        it: true
-    };
+    //Origins
+    "ORIG_EXTENSION": "Estensione",
+
+    //Bottom panel
+    "PANEL_TITLE": "Scorciatoie",
+    "PANEL_COPY_TO_DOC": "Copia nel documento corrente",
+    "PANEL_FILTER": "Filtra&hellip;"
 });
